e2e22eccad8ddf78740575808d1bed04b8883411
SurajGadkar”git config --global user.email surajgadkar14@gmail.com” <surajgadkar@Surajs-MacBook-Air.local>
2026-01-01 22:44:08 +0530

    Added mongoose connection

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
+mongoose.connect('mongodb+srv://surajgadkar14_db_user:4RDJzoHP0gpgcXgB@refresher.xbdyn8j.mongodb.net/?appName=Refresher')
+.then(() => {
+    console.log('Connected!')}
+)
+.then (()=> {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+})
+.catch(err => console.log(err))
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
 
 
